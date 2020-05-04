1c9ba40a57b0e5398cd3def5dfaeb3b7e22cef16
csc-isac <csc.isac@gmail.com>
2020-05-04 08:04:14 +0000

    docker hpfeeds x mongodb is done and can run automatically

--- simpleRun.sh
+++ simpleRun.sh
@@ -1,6 +1,7 @@
 apt-get update && apt-get upgrade -y
 apt-get install docker.io docker-compose
 
-docker-compose up -d --build
-docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "./installer.sh"
+docker-compose up -d
+docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "chmod +x installer && ./installer"
+#docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "supervisorctl status HPFeeds-broker && supervisorctl status mnemosyne"
 docker exec hpfeedsnodered_nodered_1 /bin/sh -c "pm2 start /usr/bin/node-red -- -v && ./malware/NewFile.sh"
