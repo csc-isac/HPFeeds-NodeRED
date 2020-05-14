e8bec4c666e68fd1a78fbbb56e4c9a5cd72e63d7
csc-isac <csc.isac@gmail.com>
2020-05-14 12:57:23 +0000

    add comment

--- addUser_HPFEEDS_cowrie.sh
+++ addUser_HPFEEDS_cowrie.sh
@@ -3,10 +3,16 @@
 #secret = password1234
 
 docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "cp add_user_Cowrie.sh add_user_Cowrie_cp.sh"
+#copy the add_user-Cowrie.sh script to the add_user_Cowrie_cp.sh we need this to modify the copy
 
 docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "sed -i 's/sensor_cowrie/SGU-Cow-1-Alsut/g' add_user_Cowrie_cp.sh"
+#change the ident into the ident that we want for example change ident sensor_cowrie to SGU-Cow-1-Alsut
+
 #docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "sed -i 's/password1234/PASSWORD/g' add_user_Cowrie_cp.sh"
+#change the secret into the secret that we want for example change password1234 to PASSWORD 
 
 docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "chmod u+x add_user_Cowrie_cp.sh && ./add_user_Cowrie_cp.sh"
+#change the file permission of add_user_Cowrie_cp.sh to be able to be executed and then execute the bash script
 
 docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "rm add_user_Cowrie_cp.sh"
+#remove the file 
--- addUser_HPFEEDS_dionaea.sh
+++ addUser_HPFEEDS_dionaea.sh
@@ -3,10 +3,17 @@
 #secret = password1234
 
 docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "cp add_user_Dionaea.sh add_user_Dionaea_cp.sh"
+#copy the add_user-Dionaea.sh script to the add_user_Dionaea_cp.sh we need this to modify the copy
 
 docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "sed -i 's/sensor_dionaea/SGU-Dio-2-Alsut/g' add_user_Dionaea_cp.sh"
+#change the ident into the ident that we want for example change ident sensor_dionaea to SGU-Dio-2-Alsut
+
+
 #docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "sed -i 's/password1234/PASSWORD/g' add_user_Dionaea_cp.sh"
+#change the secret into the secret that we want for example change password1234 to PASSWORD
 
 docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "chmod u+x add_user_Dionaea_cp.sh && ./add_user_Dionaea_cp.sh"
+#change the file permission of add_user_Dionaea_cp.sh to be able to be executed and then execute the bash script
 
 docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "rm add_user_Dionaea_cp.sh"
+#remove the file
