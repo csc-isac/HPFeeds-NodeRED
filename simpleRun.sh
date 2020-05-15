e70236db3508987b5addaadee2557b540153a901
csc-isac <60864614+csc-isac@users.noreply.github.com>
2020-05-15 10:15:44 +0700

    Update simpleRun.sh

--- simpleRun.sh
+++ simpleRun.sh
@@ -7,6 +7,6 @@ docker-compose up -d --build mongodb
 docker restart hpfeedsnodered_mongodb_1
 docker-compose up -d --build hpfeeds
 docker-compose up -d --build nodered
-#docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "chmod +x installer && ./installer"
-#docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "supervisorctl status HPFeeds-broker && supervisorctl status mnemosyne"
+docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "chmod +x installer && ./installer"
+docker exec hpfeedsnodered_hpfeeds_1 /bin/sh -c "supervisorctl status HPFeeds-broker && supervisorctl status mnemosyne"
 docker exec hpfeedsnodered_nodered_1 /bin/sh -c "pm2 start /usr/bin/node-red -- -v && ./malware/NewFile.sh"
